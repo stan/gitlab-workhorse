603567086d2557eaeb6e639f29bb8d2488e4fc85
Francisco Javier López <fjlopez@gitlab.com>
2019-04-10 09:55:36 +0200

    Fix problem with names

--- internal/api/service_proxy_settings_test.go
+++ internal/api/service_proxy_settings_test.go
@@ -1,77 +1,111 @@
 package api
 
 import (
-	"fmt"
 	"net/http"
-	"net/url"
-
-	"gitlab.com/gitlab-org/gitlab-workhorse/internal/helper"
+	"testing"
 )
 
-type ServiceProxySettings struct {
-	// The URL to connect to.
-	Url string
-
-	// Any headers (e.g., Authorization) to send with the request
-	Header http.Header
-
-	// The CA roots to validate the remote endpoint with, for wss:// URLs. The
-	// system-provided CA pool will be used if this is blank. PEM-encoded data.
-	CAPem string
+func service(url string) *ServiceProxySettings {
+	return &ServiceProxySettings{
+		Url: url,
+	}
 }
 
-func (t *ServiceProxySettings) URL() (*url.URL, error) {
-	return url.Parse(t.Url)
+func serviceCa(service *ServiceProxySettings) *ServiceProxySettings {
+	service = service.Clone()
+	service.CAPem = "Valid CA data"
+
+	return service
 }
 
-func (t *ServiceProxySettings) Clone() *ServiceProxySettings {
-	// Doesn't clone the strings, but that's OK as strings are immutable in go
-	cloned := *t
-	cloned.Header = helper.HeaderClone(t.Header)
-	return &cloned
+func serviceHeader(service *ServiceProxySettings, values ...string) *ServiceProxySettings {
+	if len(values) == 0 {
+		values = []string{"Dummy Value"}
+	}
+
+	service = service.Clone()
+	service.Header = http.Header{
+		"Header": values,
+	}
+
+	return service
 }
 
-func (t *ServiceProxySettings) Validate() error {
-	if t == nil {
-		return fmt.Errorf("service details not specified")
+func TestServiceClone(t *testing.T) {
+	a := serviceCa(serviceHeader(service("http:")))
+	b := a.Clone()
+
+	if a == b {
+		t.Fatalf("Address of cloned build service didn't change")
 	}
 
-	parsedURL, err := t.URL()
-	if err != nil {
-		return fmt.Errorf("invalid URL")
+	if &a.Header == &b.Header {
+		t.Fatalf("Address of cloned header didn't change")
 	}
-
-	if parsedURL.Scheme != "http" && parsedURL.Scheme != "https" {
-		return fmt.Errorf("invalid scheme: %q", parsedURL.Scheme)
-	}
-
-	return nil
 }
 
-func (t *ServiceProxySettings) IsEqual(other *ServiceProxySettings) bool {
-	if t == nil && other == nil {
-		return true
-	}
-
-	if t == nil || other == nil {
-		return false
-	}
-
-	if len(t.Header) != len(other.Header) {
-		return false
-	}
-
-	for header, values := range t.Header {
-		if len(values) != len(other.Header[header]) {
-			return false
-		}
-		for i, value := range values {
-			if other.Header[header][i] != value {
-				return false
-			}
+func TestServiceValidate(t *testing.T) {
+	for i, tc := range []struct {
+		service *ServiceProxySettings
+		valid   bool
+		msg     string
+	}{
+		{nil, false, "nil build service"},
+		{service(""), false, "empty URL"},
+		{service("ws:"), false, "websocket URL"},
+		{service("wss:"), false, "secure websocket URL"},
+		{service("http:"), true, "HTTP URL"},
+		{service("https:"), true, "HTTPS URL"},
+		{serviceCa(service("http:")), true, "any CA pem"},
+		{serviceHeader(service("http:")), true, "any headers"},
+		{serviceCa(serviceHeader(service("http:"))), true, "PEM and headers"},
+	} {
+		if err := tc.service.Validate(); (err != nil) == tc.valid {
+			t.Fatalf("test case %d: "+tc.msg+": valid=%v: %s: %+v", i, tc.valid, err, tc.service)
 		}
 	}
+}
 
-	return t.Url == other.Url &&
-		t.CAPem == other.CAPem
+func TestServiceIsEqual(t *testing.T) {
+	serv := service("http:")
+
+	servHeader2 := serviceHeader(serv, "extra")
+	servHeader3 := serviceHeader(serv)
+	servHeader3.Header.Add("Extra", "extra")
+
+	servCa2 := serviceCa(serv)
+	servCa2.CAPem = "other value"
+
+	for i, tc := range []struct {
+		serviceA *ServiceProxySettings
+		serviceB *ServiceProxySettings
+		expected bool
+	}{
+		{nil, nil, true},
+		{serv, nil, false},
+		{nil, serv, false},
+		{serv, serv, true},
+		{serv.Clone(), serv.Clone(), true},
+		{serv, service("foo:"), false},
+		{serv, service(serv.Url), true},
+		{serviceHeader(serv), serviceHeader(serv), true},
+		{servHeader2, servHeader2, true},
+		{servHeader3, servHeader3, true},
+		{serviceHeader(serv), servHeader2, false},
+		{serviceHeader(serv), servHeader3, false},
+		{serviceHeader(serv), serv, false},
+		{serv, serviceHeader(serv), false},
+		{serviceCa(serv), serviceCa(serv), true},
+		{serviceCa(serv), serv, false},
+		{serv, serviceCa(serv), false},
+		{serviceCa(serviceHeader(serv)), serviceCa(serviceHeader(serv)), true},
+		{servCa2, serviceCa(serv), false},
+	} {
+		if actual := tc.serviceA.IsEqual(tc.serviceB); tc.expected != actual {
+			t.Fatalf(
+				"test case %d: Comparison:\n-%+v\n+%+v\nexpected=%v: actual=%v",
+				i, tc.serviceA, tc.serviceB, tc.expected, actual,
+			)
+		}
+	}
 }
